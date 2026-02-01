bc307f7917570b1a9f12e6b89034b3fe63c55df8
Aryan Panwar <100417993+aryangyro@users.noreply.github.com>
2026-02-01 10:56:08 +0530

    made some improvements to the files

--- Zillow-Project/zillow.py
+++ Zillow-Project/zillow.py
@@ -3,6 +3,10 @@ from bs4 import BeautifulSoup
 import re
 import json
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 url = "https://appbrewery.github.io/Zillow-Clone/"
 
@@ -37,9 +41,42 @@ with open("properties.json", "w") as file:
     json.dump(data, file, indent=4)
 
 
-driver = webdriver.Chrome()
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
 driver.get("https://docs.google.com/forms/d/e/1FAIpQLSdl6z03H7n5tnOCJM6INQ8EkNcRVVeNxI2JWSjnk6KGKr_xTw/viewform")
 
+for item in data:
+    
+    # 1. Give the page 2 seconds to load so elements exist
+    time.sleep(2)
+
+    if item["price"] < 2500:
+    
+        address_input = driver.find_element(By.XPATH, 
+            '(//input[@type="text"])[2]')
+        address_input.send_keys(item["address"])
+        
+        # 3. Find Price Input
+        # This looks for the second text input on the page
+        price_input = driver.find_element(By.XPATH, 
+            '(//input[@type="text"])[1]')
+        price_input.send_keys(str(item["price"])) # str() ensures no errors with numbers
+        
+        # 4. Find Link Input
+        # This looks for the third text input on the page
+        link_input = driver.find_element(By.XPATH, 
+            '(//input[@type="text"])[3]')
+        link_input.send_keys(item["link"])
+
+        submit_button = driver.find_element(By.XPATH, "//span[contains(text(), 'Submit')]")
+        submit_button.click()
+        
+        time.sleep(2) # Wait for submit to finish
+
+        next_button = driver.find_element(By.LINK_TEXT, "Submit another response")
+        next_button.click()
 
 
 
